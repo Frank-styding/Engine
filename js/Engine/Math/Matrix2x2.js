0a66a366646061091620b7cb7b7203b0264efd02
Frank-Paredes <frankparedesalpi@gmail.com>
2022-02-15 10:42:21 -0500

    first commit

--- js/Engine/Math/Matrix2x2.js
+++ js/Engine/Math/Matrix2x2.js
@@ -2,6 +2,22 @@ export class Matrix2x2 {
   constructor(data = [1, 0, 0, 1]) {
     this.data = data ?? [];
   }
+
+  mul(m) {
+    let data = Matrix2x2.mul(this, m);
+    this.data = data;
+    return this;
+  }
+  scale(x, y) {
+    this.mul(Matrix2x2.scale(x, y));
+    return this;
+  }
+
+  rotate(angle) {
+    this.mul(Matrix2x2.rotate(angle));
+    return this;
+  }
+
   static mul(m, m1) {
     let d = m.data;
     let d1 = m1.data;
@@ -12,5 +28,26 @@ export class Matrix2x2 {
     d2[2] = d[2] * d1[0] + d[3] * d[2];
     d2[3] = d[2] * d1[1] + d[3] * d[3];
     return new Matrix2x2(d2);
+  }
+  static scale(x, y) {
+    return new Matrix2x2([x, 0, 0, y]);
+  }
+  static rotate(angle) {
+    let c = Math.cos(angle);
+    let s = Math.sin(angle);
+    return new Matrix2x2([c, -s, s, c]);
+  }
+  static transpose(m) {
+    let d = m.data;
+    return new Matrix2x2([d[0], d[2], d[1], d[3]]);
+  }
+  static det(m) {
+    let d = m.data;
+    return d[0] * d[3] - d[1] * d[2];
+  }
+  static inverse(m) {
+    let det = Matrix2x2.det(m);
+    let d = m.data;
+    return new Matrix2x2([d[3] / det, -d[1] / det, -d[2] / det, d[0] / det]);
   }
 }
--- js/Engine/Math/Matrix3x3.js
+++ js/Engine/Math/Matrix3x3.js
@@ -1,5 +1,57 @@
 export class Matrix3x3 {
-  constructor(data) {
+  constructor(data = [1, 0, 0, 0, 1, 0, 0, 0, 1]) {
     this.data = data;
+  }
+
+  static mul(m, m1) {
+    let d = m.data;
+    let d1 = m1.data;
+    let d2 = new Array(9).fill(0);
+    d[0] = d[0] * d1[0] + d[1] * d1[3] + d[2] * d1[6];
+    d[1] = d[0] * d1[1] + d[1] * d1[4] + d[2] * d1[7];
+    d[2] = d[0] * d1[2] + d[1] * d1[5] + d[2] * d1[8];
+
+    d[3] = d[3] * d1[0] + d[4] * d1[3] + d[5] * d1[6];
+    d[4] = d[3] * d1[1] + d[4] * d1[4] + d[5] * d1[7];
+    d[5] = d[3] * d1[2] + d[4] * d1[5] + d[5] * d1[8];
+
+    d[6] = d[6] * d1[0] + d[7] * d1[3] + d[8] * d1[6];
+    d[7] = d[6] * d1[1] + d[7] * d1[4] + d[8] * d1[7];
+    d[8] = d[6] * d1[2] + d[7] * d1[5] + d[8] * d1[8];
+  }
+  static translate(x, y) {
+    return new Matrix3x3([1, 0, x, 0, 1, y, 0, 0, 1]);
+  }
+  static scale(x, y, z = 1) {
+    return new Matrix3x3([x, 0, 0, 0, y, 0, 0, 0, z]);
+  }
+  static rotateX(angle) {
+    let c = Math.cos(angle);
+    let s = Math.sin(angle);
+    return new Matrix3x3([1, 0, 0, 0, c, -s, 0, s, c]);
+  }
+  static rotateY(angle) {
+    let c = Math.cos(angle);
+    let s = Math.sin(angle);
+    return new Matrix3x3([c, 0, s, 0, 1, 0, -s, 0, c]);
+  }
+  static rotateZ(angle) {
+    let c = Math.cos(angle);
+    let s = Math.sin(angle);
+    return new Matrix3x3([c, -s, 0, s, c, 0, 0, 0, 1]);
+  }
+  static transpose(m) {
+    let d = m.data;
+    return new Matrix3x3([
+      d[0],
+      d[3],
+      d[6],
+      d[1],
+      d[4],
+      d[7],
+      d[2],
+      d[5],
+      d[8],
+    ]);
   }
 }
